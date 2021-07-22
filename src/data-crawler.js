c8857e261ba5e71829a6fdaa03fbf82df983f6e9
IanMacfarlane <iandmacfarlane13@gmail.com>
2021-07-21 22:17:18 -0600

    get pokemon

--- src/data-crawler.js
+++ src/data-crawler.js
@@ -4,32 +4,65 @@
 
 let request = require('request');
 
-let url = 'https://pokemondb.net/pokedex';
-
 let pokemonData = {};
+let url = 'https://pokemondb.net/pokedex/';
+getGames(url);
 
-request(
-	{ uri: url },
-	function(error, response, body) {
 
-		// TODO get all pokemon games
-
-		let keyword = '<strong>National Dex</strong>';
-		let index = body.search(keyword);
-		body = body.slice(index);
-
-		pokemonData.games = [];
-		
-		keyword = '/pokedex/game/';
-		while (body.search(keyword) != -1) {
+function getPokemon(game) {
+	request(
+		{ uri: url + 'game/' + game + '/' },
+		function(error, response, body) {
+			keyword = 'infocard-list';
 			index = body.search(keyword);
-			body = body.slice(index + keyword.length);
-			index = body.search('\"')
-			pokemonData.games.push(body.slice(0, index));
+			body = body.slice(index);
+
+			pokemonData.games[game].pokemon = [];
+
+			keyword = 'name" href=\"/pokedex/';
+			while (body.search(keyword) != -1) {
+				index = body.search(keyword);
+				body = body.slice(index + keyword.length);
+				index = body.search('\"');
+				let pokemon = body.slice(0, index);
+				//console.log(pokemon);
+				pokemonData.games[game].pokemon.push(pokemon);
+			}
+
+			console.log(pokemonData);
+			console.log(pokemonData.games[game].pokemon);
+			
 		}
+	);
+}
 
-		console.log(pokemonData);
-		
-	}
-);
 
+function getGames(url) {
+	request(
+		{ uri: url },
+		function(error, response, body) {
+
+			// get all pokemon games
+
+			let keyword = '<strong>National Dex</strong>';
+			let index = body.search(keyword);
+			body = body.slice(index);
+
+			pokemonData.games = {};
+			
+			keyword = '/pokedex/game/';
+			while (body.search(keyword) != -1) {
+				index = body.search(keyword);
+				body = body.slice(index + keyword.length);
+				index = body.search('\"');
+				let game = body.slice(0, index);
+				pokemonData.games[game]= {};
+			}
+
+			/*for (game in pokemonData.games) {
+				getPokemon(game);
+			}*/
+			getPokemon('firered-leafgreen');
+		}
+	);
+}
